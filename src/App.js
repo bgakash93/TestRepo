eaa63d39cb198e95b708896268e1988b2d34d7a0
AKASH GANESH <akash.ganesh@mercedes-benz.com>
2024-09-29 12:45:38 +0530

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello MBSE!
+          Hello MBSE-Team Binoj!
         </p>
       </header>
     </div>
