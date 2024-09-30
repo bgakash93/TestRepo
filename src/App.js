728758997476c01f752ec412ffc221ee35f833d1
AKASH GANESH <akash.ganesh@mercedes-benz.com>
2024-09-30 19:43:44 +0530

    Testing Dev and INT

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello MBSE-Team Binoj!--
+          Hello MBSE
         </p>
       </header>
     </div>
